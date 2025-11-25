2c5388f653be7c2c977c629e698aa525ca26f414
EC2 Default User <ec2-user@ip-172-31-84-110.ec2.internal>
2025-11-25 23:00:40 +0000

    Ajustando configuracoes

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "clc14-jnr-terraform"
+    bucket = "jnr-terraform-clc14"
     key    = "network/terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true
--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 variable "vpc_name" {
   type = string
+  default = "vpc-terraform"
 }
 
 resource "aws_vpc" "minha_vpc" {
@@ -7,7 +8,7 @@ resource "aws_vpc" "minha_vpc" {
   instance_tenancy = "default"
 
   tags = {
-    Name = "var.vpc_name"
+    Name = var.vpc_name
   }
 
 }
