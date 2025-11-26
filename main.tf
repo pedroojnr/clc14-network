beab67d7e4463408d82019d13a509859b2e6c6e1
PEDRO JNR <pedro.sjunior@aluno.faculdadeimpacta.com.br>
2025-11-26 00:45:07 +0000

    Corrigindo vulnerabilidade

--- main.tf
+++ main.tf
@@ -10,8 +10,25 @@ resource "aws_vpc" "minha_vpc" {
   tags = {
     Name = var.vpc_name
   }
-
 }
+
+# Correcao primeira issue
+resource "aws_flow_log" "example" {
+  log_destination      = "arn:aws:s3:::jnr-terraform-clc14"
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.minha_vpc.id
+}
+
+# Correcao segunda issue
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.minha_vpc.id
+  
+  tags = {
+    Name = "my-iac-sg"
+  }
+}
+
 
 ## Cria subnet privada na us-east-1a
 resource "aws_subnet" "private_subnet_1a" {
